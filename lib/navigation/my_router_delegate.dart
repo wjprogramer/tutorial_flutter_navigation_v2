805f53220f601ba3475d685757076bf8ac0186bb
jay_wu <jay_wu>
2021-04-24 14:39:35 +0800

    🚧 store current MyRouteConfig

--- lib/navigation/my_router_delegate.dart
+++ lib/navigation/my_router_delegate.dart
@@ -6,11 +6,13 @@ import 'my_route_config.dart';
 class MyRouterDelegate extends RouterDelegate<MyRouteConfig>
     with ChangeNotifier, PopNavigatorRouterDelegateMixin<MyRouteConfig> {
 
+  MyRouteConfig _currentConfiguration;
+
   @override
   GlobalKey<NavigatorState> get navigatorKey => throw UnimplementedError();
 
   @override
-  MyRouteConfig get currentConfiguration => throw UnimplementedError();
+  MyRouteConfig get currentConfiguration => _currentConfiguration;
 
   @override
   Widget build(BuildContext context) {
@@ -18,8 +20,9 @@ class MyRouterDelegate extends RouterDelegate<MyRouteConfig>
   }
 
   @override
-  Future<void> setNewRoutePath(MyRouteConfig newState) async {
-    throw UnimplementedError();
+  Future<void> setNewRoutePath(MyRouteConfig newMyRouteConfig) async {
+    _currentConfiguration = newMyRouteConfig;
+    return;
   }
 
 }
