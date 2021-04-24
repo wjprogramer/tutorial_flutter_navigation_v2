945a3ce00ce7fa9f5ae57df72ab079b7b3caac03
jay_wu <jay_wu>
2021-04-24 16:13:50 +0800

    setPathUrlStrategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'app/app.dart';
 
 void main() {
+  setPathUrlStrategy();
+
   runApp(MyApp());
 }
