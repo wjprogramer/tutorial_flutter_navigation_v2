e6876db8ce0aa4c74ce674f4e33fa49ddd45869d
jay_wu <jay_wu>
2021-04-24 15:19:09 +0800

    wrap main content with router

--- lib/layout/base_layout.dart
+++ lib/layout/base_layout.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tutorial_flutter_navigation_v2/layout/base_side_bar.dart';
+import 'package:tutorial_flutter_navigation_v2/navigation/my_route_information_parser.dart';
+import 'package:tutorial_flutter_navigation_v2/navigation/my_router_delegate.dart';
 import 'package:tutorial_flutter_navigation_v2/screens/simple_screen.dart';
 import "package:universal_html/html.dart" as html;
 import 'package:flutter/foundation.dart' show kIsWeb;
@@ -10,6 +12,31 @@ class BaseLayout extends StatefulWidget {
 }
 
 class _BaseLayoutState extends State<BaseLayout> {
+
+  PlatformRouteInformationProvider _routeInformationProvider;
+
+  MyRouterDelegate _routerDelegate;
+  MyRouteInformationParser _routeInformationParser = MyRouteInformationParser();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _routeInformationProvider = PlatformRouteInformationProvider(
+        initialRouteInformation: RouteInformation(
+          location: '/about',
+        )
+    );
+
+    _routerDelegate = MyRouterDelegate();
+  }
+
+  @override
+  void dispose() {
+    _routeInformationProvider.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +44,11 @@ class _BaseLayoutState extends State<BaseLayout> {
         children: [
           BaseSideBar(),
           Expanded(
-            child: SimpleScreen(text: 'Home')
+            child: Router(
+              routerDelegate: _routerDelegate,
+              routeInformationParser: _routeInformationParser,
+              routeInformationProvider: _routeInformationProvider,
+            )
           ),
         ],
       ),
